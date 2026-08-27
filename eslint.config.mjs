da1ddd7360518911e6139da9e15fd1f5cbe369a4
Simo-J-E <128783338+Simo-J-E@users.noreply.github.com>
2026-08-27 10:35:29 +0300

    Refactor ESLint configuration and update package.json dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -32,14 +31,12 @@ export default tseslint.config(
     },
     plugins: {
       "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
     },
     rules: {
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "react-refresh/only-export-components": "off",
     },
   },
   {
